1ae03c0850e1648a8b600c3c2e050876ee97f012
Simon Bailey <30531868+dev-simonbailey@users.noreply.github.com>
2024-10-08 13:54:22 +0100

    creating pages.

--- assets/js/environment.js
+++ assets/js/environment.js
@@ -2,9 +2,8 @@ function updateImageSources(isDev) {
     const images = document.querySelectorAll("img");
     images.forEach(img => {
       let src = img.getAttribute("src");
-      if (isDev) {
-        src = src.replace("/documentation/assets/img/", "/assets/img/");
-      } else {
+      console.log(src);
+      if (!isDev) {
         src = src.replace("/assets/img/", "/documentation/assets/img/");
       }
       img.setAttribute("src", src);
